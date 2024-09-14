54818e98fe46b4ce71ea98705ee7f8d59b1a4323
Junior <ernesto@.com>
2024-09-14 00:21:25 -0400

    views create,delete,complete

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=300, blank=False, null=False)
+    date = models.DateField(auto_now=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from .form import TaskForm
+from .models import Task
 
-# Create your views here.
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, "index.html", {"form": TaskForm, "tasks": tasks})
+
+
+def create(request):
+    form = TaskForm()
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    return redirect("/")
+
+
+def read(request):
+    pass
+
+
+def update(request, pk):
+    task = Task.objects.get(pk=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    else:
+        form = TaskForm(instance=task)
+    return render(request, "update.html", {"form": form})
+
+
+def delete(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.delete()
+    return redirect("/")
+
+def complete(request,pk):
+    task = Task.objects.get(pk=pk)
+    task.status = True
+    print(task.status)
+    task.save()
+    return redirect('/')
